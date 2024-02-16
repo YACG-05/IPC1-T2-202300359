274ef2ea2f7c0cdcaaed1fcd0e9b3ee61744c827
YACG-05 <3186186720501@ingenieria.usac.edu.gt>
2024-02-15 22:27:21 -0600

    Mejora de estructura

--- src/ipc1/t2/pkg202300359/IPC1T2202300359.java
+++ src/ipc1/t2/pkg202300359/IPC1T2202300359.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package ipc1.t2.pkg202300359;
 
 import java.util.Scanner;
@@ -12,9 +8,6 @@ import java.util.Scanner;
  */
 public class IPC1T2202300359 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String continuar; 
